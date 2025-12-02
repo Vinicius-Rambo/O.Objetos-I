d6b4b25c97f4124001b018ba659d71987117d738
Vinicius Rambo <vncs.rambo@gmail.com>
2025-12-02 20:03:33 -0300

    Delete AvaEx3 directory

--- AvaEx3/ImportacaoArquivos.java
+++
@@ -1,5 +0,0 @@
-public interface ImportacaoArquivos { //Interface do sistema.
-    void carregarConfiguracoes(String arqConfig) throws Exception;
-    void importarDados(String arqDados) throws Exception;
-
-}
